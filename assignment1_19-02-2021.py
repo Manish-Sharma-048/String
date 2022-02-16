bc91a1d9671761b5e187c03581bf1a5e8d230855
Manish Sharma <99163884+Manish-Sharma-048@users.noreply.github.com>
2022-02-16 12:19:56 +0530

    Add files via upload

---
+++ assignment1_19-02-2021.py
@@ -0,0 +1,33 @@
+# 1) Write a Python program to print the following string in a specific format.
+print("Output 1:- \n")
+
+print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are")
+print("\n\t\t\t\tx--------------------x\n\n")
+
+# 2) WAP to print names of your 5 friends each in new line using single print() and single string
+print("Output 2:- \n")
+
+print("1. Anmol\n2. Ankit\n3. Rajeev\n4. Sumit\n5. Karan")
+print("\n\t\t\t\tx--------------------x\n\n")
+
+# 3) WAP to print names of your 5 friends each in new line using single print() and independent string for each name
+print("Output 3:- \n")
+
+print("1. Anmol","2. Ankit","3. Rajeev","4. Sumit","5. Karan", sep = "\n")
+print("\n\t\t\t\tx--------------------x\n\n")
+
+# 4) WAP to print names of your 5 friends each in new line using 5 print()
+print("Output 4:- \n")
+
+print("1. Anmol")
+print("2. Ankit")
+print("3. Rajeev")
+print("4. Sumit")
+print("5. Karan")
+print("\n\t\t\t\tx--------------------x\n\n")
+
+#5) WAP to print \t and \n as it is in the output
+print("Output 5:- \n")
+
+print(r"\tHello\n\tWorld")
+print("\n\t\t\t\tx--------------------x")
